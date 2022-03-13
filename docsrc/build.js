eb8c2ab5230d3f8aa3afceba8ee711d687adc693
jelmerveen <jelmerveen@pennions.com>
2022-03-13 16:07:16 +0100

    docs: add partial in partial resolving, added dev-comment possibility; fix: some margin and paddings; chore: added prism for codeblocks

--- docsrc/build.js
+++ docsrc/build.js
@@ -8,11 +8,15 @@ const writeFile = util.promisify(fs.writeFile);
 
 
 (async function () {
-    const vm = { partials: {} };
 
+    const viewmodelDir = './docsrc/viewmodel/';
     const partialsDir = './docsrc/partials';
     const templatesDir = './docsrc/templates';
     const outputDir = './docs/';
+
+    const vmString = await readFile(`${viewmodelDir}/viewmodel.json`).catch(error => console.log(error));
+    const vm = JSON.parse(vmString);
+
 
     const partialfileNames = await readDirectory(partialsDir).catch(error => console.log(error));
     const templatefileNames = await readDirectory(templatesDir).catch(error => console.log(error));
@@ -23,10 +27,18 @@ const writeFile = util.promisify(fs.writeFile);
         vm.partials[partialName] = fileContents.toString();
     }
 
+    // Resolve partial in partials.
+    for (const partial of Object.keys(vm.partials)) {
+        vm.partials[partial] = jet.compile(vm.partials[partial], vm);
+    }
+
     for (const fileName of templatefileNames) {
         const fileContents = await readFile(`${templatesDir}/${fileName}`).catch(error => console.log(error));
         const template = fileContents.toString();
-        await writeFile(`${outputDir}/${fileName}`, jet.compile(template, vm));
+
+        renderTemplate = template.replace(/<!--dev[.\s\S]*end-->/gmi, "");
+
+        await writeFile(`${outputDir}/${fileName}`, jet.compile(renderTemplate, vm));
     }
 })();
 
