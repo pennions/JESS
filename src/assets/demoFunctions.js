ccf7d644d8c533377cbde424285ce06e40b9de15
jelmerveen <jelmerveen@pennions.com>
2023-06-19 23:02:33 +0200

    chore: remove console logs

--- src/assets/demoFunctions.js
+++ src/assets/demoFunctions.js
@@ -2,7 +2,6 @@ export function setupNotificationTrigger(buttonId, notificationId) {
 
     document.getElementById(buttonId).addEventListener('click', () => {
         const notificationElement = document.getElementById(notificationId);
-        console.log(buttonId, notificationId);
 
         notificationElement.querySelector('.close').addEventListener('click', (e) => {
             e.target.parentNode.removeAttribute('open');
